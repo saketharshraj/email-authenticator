b9870babe7ad1a807fdb928bff722b7915f079a6
Harsh Raj <saketharshraj@gmail.com>
2022-03-25 06:47:23 +0530

    Email routes created

---
+++ routes/emailAuthRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const routes = express.Router();
+const verifyEmail = require("../../controllers/emails/emailVerfier");
+const sendEmail = require("../../controllers/emails/sendEmail");
+
+routes.post("/verify-email", verifyEmail);
+
+routes.post("/send-email", sendEmail);
+
+module.exports = routes;
